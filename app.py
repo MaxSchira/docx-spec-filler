4be7abc030f75ab09f9a460d4db71b9c5d635010
MaxSchira <max.schira@web.de>
2025-04-05 18:19:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, send_file
+from docx import Document
+import tempfile
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def health():
+    return 'Server is alive!'
+
+@app.route('/fill-doc', methods=['POST'])
+def fill_doc():
+    data = request.json["data"]
+    template_path = "00000_Specification_Extract Vorlage_V2.0.docx"
+    doc = Document(template_path)
+
+    for para in doc.paragraphs:
+        for key, value in data.items():
+            if key in para.text:
+                para.text = para.text.replace(key, value)
+
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for key, value in data.items():
+                    if key in cell.text:
+                        cell.text = cell.text.replace(key, value)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as tmp:
+        doc.save(tmp.name)
+        return send_file(tmp.name, as_attachment=True, download_name="demo_spec_filled.docx")
+
+if __name__ == "__main__":
+    app.run()
