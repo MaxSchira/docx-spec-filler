6c0852defc53d5d85c6ebe9af4f828af26ca72b1
MaxSchira <max.schira@web.de>
2025-04-13 17:05:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,31 +6,30 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def health():
-    return "Server is alive!"
-
 @app.route("/fill-doc", methods=["POST"])
 def fill_doc():
     try:
-        # Form-Daten abrufen
+        # Nutze Form-Data Input
         raw_json = request.form.get("data")
         if not raw_json:
-            return "Missing 'data' in form submission", 400
-        
+            return "Missing 'data' in form", 400
+
         data = json.loads(raw_json)
 
-        # Template laden und rendern
+        # Template laden & befüllen
         doc = DocxTemplate("Extract_Template.docx")
         doc.render(data)
 
-        # Temporäre Datei speichern und senden
         with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as tmp:
             doc.save(tmp.name)
             return send_file(tmp.name, as_attachment=True, download_name="filled_specification.docx")
 
     except Exception as e:
         return f"Error processing document: {str(e)}", 500
+
+@app.route("/")
+def health():
+    return "Server is alive!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
